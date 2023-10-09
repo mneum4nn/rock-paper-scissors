311209f3a2b05d2bd88119b8a67434dccdd1b1b2
mneum4nn <n3um4nn@proton.me>
2023-10-09 16:58:30 -0600

    Include an end of game message once either player accumulates 5 points

--- game_functions.js
+++ game_functions.js
@@ -1,10 +1,10 @@
 let playerPoints = 0;
 let computerPoints = 0;
-let rock = document.querySelector(".rock");
-let paper = document.querySelector(".paper");
-let scissors = document.querySelector(".scissors");
 
-
+let rock = document.querySelector('#rock');
+let paper = document.querySelector('#paper');
+let scissors = document.querySelector('#scissors');
+let outcome = document.querySelector(".round-outcome");
 
 function getComputerChoice (){
     let options = ['rock','paper','scissors'];
@@ -13,7 +13,6 @@ function getComputerChoice (){
 }
 
 function playRound (playerSelection, computerSelection){
-    let outcome = document.querySelector(".round-outcome"); 
     switch (playerSelection) {
         case 'rock':
             if (computerSelection == 'rock'){
@@ -23,14 +22,12 @@ function playRound (playerSelection, computerSelection){
             }
             else if (computerSelection=='paper'){
                 const p = document.createElement('p');
-
                 p.innerText = "You lose! Paper beats rock";
                 outcome.appendChild(p);
                 computerPoints++;
             }
             else {
                 const p = document.createElement('p');
-
                 playerPoints++;
                 p.innerText="You win! Rock beats scissors";
                 outcome.appendChild(p);
@@ -76,23 +73,56 @@ function playRound (playerSelection, computerSelection){
             } 
             break;            
     }
+}       
+const winner = document.querySelector('.winner');
+
+function endOfGame (){
+    if (playerPoints == 5){
+        const txt = document.createElement(h2);
+        winner.style.display = "block";  
+        txt.innerText = "Congrats! You Win !";
+        winner.appendChild(txt);
+        playerPoints = 0;
+        computerPoints = 0;
+    }
+    else if (computerPoints == 5){
+        const txt = document.createElement('h2');
+        winner.style.display = "block";  
+        txt.innerText="Sorry, better luck next time !";
+        winner.appendChild(txt);
+        playerPoints = 0;
+        computerPoints = 0;  
+    }
+
+    }
+
+if (rock){
+    rock.addEventListener('click', () => {
+        const playerSelection = 'rock';
+        const computerSelect = getComputerChoice();
+        playRound(playerSelection,computerSelect);
+        endOfGame();
+    }); 
 }
 
-rock.addEventListener('click', () => {
-    const playerSelection = 'rock';
-    const computerSelect = getComputerChoice();
-    return playRound(playerSelection,computerSelect);
-    })
+if (scissors){
+    scissors.addEventListener('click', () => {
+        const playerSelection = 'scissors';
+        const computerSelect = getComputerChoice();
+        playRound(playerSelection,computerSelect);
+        endOfGame();
+    });
 
-scissors.addEventListener('click', () => {
-    const playerSelection = 'scissors';
-    const computerSelect = getComputerChoice();
-    return playRound(playerSelection,computerSelect);
-    })
-paper.addEventListener('click', () => {
-    const playerSelection = 'paper';
-    const computerSelect = getComputerChoice();
-    playRound(playerSelection,computerSelect);
-    })
+}
+
+if (paper){
+    paper.addEventListener('click', () => {
+        const playerSelection = 'paper';
+        const computerSelect = getComputerChoice();
+        playRound(playerSelection,computerSelect);
+        endOfGame();
+    });
+    }
+
 
 
